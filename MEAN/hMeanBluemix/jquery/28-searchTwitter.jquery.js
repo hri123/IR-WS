55a4295d127f3ed8c938bfcf2eaee48a931f083b
Hrishikesh <hri123@gmail.com>
2014-10-02 20:48:51 +0530

    NA

--- MEAN/hMeanBluemix/jquery/28-searchTwitter.jquery.js
+++ MEAN/hMeanBluemix/jquery/28-searchTwitter.jquery.js
@@ -1,4 +1,4 @@
-// Utility
+// Utility - Object.create method does not exist in older browsers, so creating it incase not present
 if ( typeof Object.create !== 'function' ) {
 	Object.create = function( obj ) {
 		function F() {};
@@ -7,7 +7,7 @@ if ( typeof Object.create !== 'function' ) {
 	};
 }
 
-(function( $, window, document, undefined ) {
+(function( $, window, document, undefined ) { // undefined is passed to make sure nobody before has overridden undefined
 	var Twitter = {
 		init: function( options, elem ) {
 			var self = this;
@@ -22,6 +22,7 @@ if ( typeof Object.create !== 'function' ) {
 				? options
 				: options.search;
 
+			// dojo.mixin
 			self.options = $.extend( {}, $.fn.queryTwitter.options, options );
 
 			self.refresh( 1 );
@@ -32,7 +33,7 @@ if ( typeof Object.create !== 'function' ) {
 
 			setTimeout(function() {
 				self.fetch().done(function( results ) {
-					results = self.limit( results.results, self.options.limit );
+					results = self.limit( results, self.options.limit );
 
 					self.buildFrag( results );
 
@@ -53,7 +54,7 @@ if ( typeof Object.create !== 'function' ) {
 			return $.ajax({
 				url: this.url1 + this.search + this.url2,
 				data: {},
-				dataType: 'jsonp'
+				dataType: ''
 			});
 		},
 
@@ -61,7 +62,7 @@ if ( typeof Object.create !== 'function' ) {
 			var self = this;
 
 			self.tweets = $.map( results, function( obj, i) {
-				return $( self.options.wrapEachWith ).append ( obj.text )[0];
+				return $( self.options.wrapEachWith ).append ( obj.archive_url);
 			});
 		},
 
@@ -82,16 +83,23 @@ if ( typeof Object.create !== 'function' ) {
 		}
 	};
 
+	// plugin is created by extending jQuery's prototype, like below
 	$.fn.queryTwitter = function( options ) {
-		return this.each(function() {
-			var twitter = Object.create( Twitter );
+		// in this scope this refers to the jQuery object, so doing $(this) is not required
+		return this.each(function() { // return is useful for chaining
+			var twitter = Object.create( Twitter ); // prototype
+			console.log(Twitter); // Object
+			console.log(twitter); // instance 
 			
 			twitter.init( options, this );
 
-			$.data( this, 'queryTwitter', twitter );
+			// saving a copy of the instance if the user wants to modify
+			$.data( this, 'queryTwitter', twitter ); // this is jQuery.data - http://api.jquery.com/jquery.data/
+			// $(this).data('queryTwitter', twitter ); // http://api.jquery.com/data/, this is supposedly slower than $.data
 		});
 	};
 
+// options externalized so that it can be easily changed by user using it
 	$.fn.queryTwitter.options = {
 		search: 'douglascrockford',
 		wrapEachWith: '<li></li>',
@@ -101,4 +109,4 @@ if ( typeof Object.create !== 'function' ) {
 		transition: 'fadeToggle'
 	};
 
-})( jQuery, window, document );
+})( jQuery, window, document ); // wrap in self executing function to avoid clashes for what '$' sign refers to
