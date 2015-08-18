fa5ffe2dc056fc07f6463d88e6366a853441cbc2
hri123 <hri123@gmail.com>
2015-08-18 15:41:54 +0530

    Removed server js files from karma configuration

--- MEAN/Angular/ngTutorial/ng-rb/test/karma.conf.js
+++ MEAN/Angular/ngTutorial/ng-rb/test/karma.conf.js
@@ -10,7 +10,10 @@ module.exports = function(config){
       'bower_components/angular-animate/angular-animate.js',
       'bower_components/angular-mocks/angular-mocks.js',
       'bower_components/mobile-angular-ui/dist/js/mobile-angular-ui.js',
-      'app/js/**/*.js',
+      'bower_components/angular-xeditable/dist/js/xeditable.js',
+      'bower_components/socket.io-client/socket.io.js', // w/o this -> ReferenceError: io is not defined
+      'app/js/common/**/*.js',
+      'app/js/ui/**/*.js',
       'test/unit/**/*.js'
     ],
 
