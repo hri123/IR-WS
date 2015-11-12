3220765295cf72075f072e5da854b256ed970b8f
HRISHIKESH S. KUMAR <hrishikesh.kumar@in.ibm.com>
2015-11-12 17:30:17 +0530

    BuildYourOwnAngularJS-InitialCut

---
+++ MEAN/Angular/ngTutorial/ng-rb/bld_yr_ng/src/scope.js
@@ -0,0 +1,27 @@
+/* jshint globalstrict: true */
+'use strict';
+
+function Scope() {
+
+	this.$$watchers = []; // $$ signifies that this variable should be considered private
+}
+
+Scope.prototype.$watch = function(watchFn, listenerFn) {
+	var watcher = {
+		watchFn: watchFn,
+		listenerFn: listenerFn || function() {}
+	};
+	this.$$watchers.push(watcher);
+};
+
+Scope.prototype.$digest = function() {
+	var self = this;
+	this.$$watchers.forEach(function(watcher) {
+		var newValue = watcher.watchFn(self);
+		var oldValue = watcher.last;
+		if (newValue !== oldValue) {
+			watcher.listenerFn(newValue, oldValue, self);
+			watcher.last = newValue;
+		}
+	});
+};
