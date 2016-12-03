e2a4cc9c47d715d5aa6f927beffd0ad351f4b4fa
hrishikesh-kumar <hrishikesh.kumar@in.ibm.com>
2016-12-03 11:52:47 +0530

    Deleted .../MEAN/Angular/ngTutorial/ng-rb

--- MEAN/Angular/ngTutorial/ng-rb/app/js/server/config.js
+++
@@ -1,7 +0,0 @@
-var ids = {
-    paths: {
-        appDir: 'app/', // '../../'
-        storageClient_local_baseDirectory: '/Users/hrishikesh/H/HP/Dropbox/Kaizen/ng-rb/RB-files/'
-    }
-}
-module.exports = ids
