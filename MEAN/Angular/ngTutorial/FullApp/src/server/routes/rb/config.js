eb4ac7a27133be768a5f5e013d375be2020de144
Hrishikesh Kumar <hrishikesh.kumar@in.ibm.com>
2020-11-16 21:31:44 +0530

    Update config.js

--- MEAN/Angular/ngTutorial/FullApp/src/server/routes/rb/config.js
+++ MEAN/Angular/ngTutorial/FullApp/src/server/routes/rb/config.js
@@ -1,7 +1,8 @@
 var ids = {
     paths: {
         appDir: 'app/', // '../../'
-        storageClient_local_baseDirectory: '/Users/hrishikesh/H/HP/Dropbox/Kaizen/ng-rb/RB-files/'
+        // storageClient_local_baseDirectory: '/Users/hrishikesh/H/HP/Dropbox/Kaizen/ng-rb/RB-files/'
+        storageClient_local_baseDirectory: '/Volumes/Kaizen/ng-rb/RB-files/'
     }
 }
 module.exports = ids
